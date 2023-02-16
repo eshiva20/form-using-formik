8702fc73c460aac8b5c91d6c343b357f396c3d7e
shiva <shiva@nimapinfotech.com>
2023-02-16 18:21:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,77 +1,92 @@
 import React from "react";
-import "./App.css";
 import { useFormik } from "formik";
-import * as Yup from "yup";
+import { useState } from "react";
+import * as Yup from "yup"
 
-const initialValues = {
-  name: "",
-  email: "",
-  contact: "",
-};
-const registrationSchema = Yup.object({
-  name: Yup.string()
-    .min(3, "minimum 3 char required")
-    .max(15, "max 15 chars allowed")
-    .required("required !"),
-  email: Yup.string().email().required("required !"),
-  contact: Yup.string().min(10,"enter valid phone Number").max(10,"enter valid phone Number").required("required !"),
-});
 const App = () => {
-  const { values, errors, touched, handleChange, handleSubmit, handleBlur } =
-    useFormik({
-      initialValues: initialValues,
-      validationSchema: registrationSchema,
-      onSubmit: (values) => {
-        console.log("values", values);
-      },
-    });
+  const[data,setData]=useState([])
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      contact: "",
+      gender: "",
+      country: "",
+    },
+    validationSchema:Yup.object({
+      username:Yup.string().max(10,"length should be less then 10").required("its required"),
+      email:Yup.string().required("its required"),
+      contact:Yup.number().min(10).max(10).required("its Required"),
+      gender:Yup.string(),
+      country:Yup.string().required("its required")
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  });
+  
+
 
   return (
     <div>
-      <h1 className="title">Contact Dairy</h1>
-      <div className="main">
-        <form onSubmit={handleSubmit} className="form">
-          <div className="form-elem">
-            <label htmlFor="name">Name :</label>
-            <input
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.name}
-              name="name"
-              id="name"
-              type="text"
-            />
-            {touched.name && errors.name ? <span>{errors.name}</span> : null}
-          </div>
-          <div className="form-elem">
-            <label htmlFor="email">Email :</label>
-            <input
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-              name="email"
-              id="email"
-              type="email"
-            />
-            {touched.email && errors.email ? <span>{errors.email}</span> : null}
-          </div>
-          <div className="form-elem">
-            <label htmlFor="contact">Contact :</label>
-            <input
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.contact}
-              name="contact"
-              id="contact"
-              type="text"
-            />
-            {touched.contact && errors.contact ? (
-              <span>{errors.name}</span>
-            ) : null}
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+      <h1>React formik </h1>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Enter Name"
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          value={formik.values.username}
+        />
+        {formik.touched.username && formik.errors.username?<p style={{color:"red"}}>{formik.errors.username}</p>:null}  
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email id"
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email?<p style={{color:"red"}}>{formik.errors.email}</p>:null}  
+        <input
+          type="text"
+          name="contact"
+          placeholder="Enter Contact"
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          value={formik.values.contact}
+        />
+        {formik.touched.contact && formik.errors.contact?<p style={{color:"red"}}>{formik.errors.contact}</p>:null}  
+        <h2>Gender</h2>
+
+        <input
+          type="radio"
+          name="gender"
+          value="Male"
+          onChange={formik.handleChange}
+        />
+        <label>Male</label>
+
+        <input
+          type="radio"
+          name="gender"
+          value="Female"
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+        />
+        <label>Female</label>
+
+        <select name="country" onBlur={formik.handleBlur} onChange={formik.handleChange}>
+          <option value="">Select Country</option>
+          <option value="India">India</option>
+          <option value="America">America</option>
+          <option value="France">France</option>
+          <option value="Germany">Germany</option>
+        </select>
+        {formik.touched.country && formik.errors.country?<p style={{color:"red"}}>{formik.errors.country}</p>:null}  
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
