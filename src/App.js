6ebfcd1e0a2cc060b4c1322dfab7fab007e55fe8
shiva <shiva@nimapinfotech.com>
2023-02-17 18:51:53 +0530

    Added Yup Validation And Create json server

--- src/App.js
+++ src/App.js
@@ -1,90 +1,173 @@
-import React from "react";
 import { useFormik } from "formik";
-import { useState } from "react";
-import * as Yup from "yup"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import * as Yup from "yup";
+import axios from "axios";
 
 const App = () => {
-  const[data,setData]=useState([])
+  const [data, setData] = useState([]);
+  const [userinput, setUserInput] = useState({});
+  const emailregex =
+    /^([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)@([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)[\\.]([a-zA-Z]{2,9})$/;
+  const contactRegex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
+
   const formik = useFormik({
     initialValues: {
-      username: "",
+      id: "",
+      name: "",
       email: "",
       contact: "",
       gender: "",
-      country: "",
+      state: "0",
     },
-    validationSchema:Yup.object({
-      username:Yup.string().max(10,"length should be less then 10").required("its required"),
-      email:Yup.string().required("its required"),
-      contact:Yup.number().min(10).max(10).required("its Required"),
-      gender:Yup.string(),
-      country:Yup.string().required("its required")
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .min(3, "min 3 char required")
+        .max(10, "max 10 chars allowed")
+        .required("Name Required"),
+      email: Yup.string()
+        .matches(emailregex, "Enter valid mail id")
+        .required("Please Enter your email"),
+      contact: Yup.string()
+        .matches(contactRegex, "Enter Correct Contact number ")
+        .required("Contact Required"),
+      state: Yup.string().required("Select State"),
     }),
-    onSubmit: (values) => {
-      console.log(values)
+    onSubmit: (values, { setFieldValues }) => {
+      setUserInput({
+        name: values.name,
+        email: values.email,
+        contact: values.contact,
+        gender: values.gender,
+        state: values.state,
+      });
+      setFieldValues("gender", "Male");
+      console.log("values", values);
     },
   });
-  
 
+  const handleAdd = async () => {
+    await axios
+      .post("http://localhost:8080/posts", userinput)
+      .then(() => {
+        alert("User Registered");
+
+        console.log(formik.values);
+        formik.values.contact = "";
+        formik.values.name = "";
+        formik.values.email = "";
+        formik.values.gender = "Male";
+        formik.values.state = "0";
+      })
+      .catch((err) => console.log(err));
+  };
+
+  if (Object.values(userinput).length !== 0) {
+    handleAdd();
+    setUserInput({});
+  }
+
+  const getData = () => {
+    axios
+      .get("http://localhost:8080/posts")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log("getData error", err));
+  };
+
+  useEffect(() => {
+    getData();
+  }, [userinput]);
 
   return (
-    <div>
-      <h1>React formik </h1>
-      <form onSubmit={formik.handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Enter Name"
-          onBlur={formik.handleBlur}
-          onChange={formik.handleChange}
-          value={formik.values.username}
-        />
-        {formik.touched.username && formik.errors.username?<p style={{color:"red"}}>{formik.errors.username}</p>:null}  
-        <input
-          type="email"
-          name="email"
-          placeholder="Enter Email id"
-          onBlur={formik.handleBlur}
-          onChange={formik.handleChange}
-          value={formik.values.email}
-        />
-        {formik.touched.email && formik.errors.email?<p style={{color:"red"}}>{formik.errors.email}</p>:null}  
-        <input
-          type="text"
-          name="contact"
-          placeholder="Enter Contact"
-          onBlur={formik.handleBlur}
-          onChange={formik.handleChange}
-          value={formik.values.contact}
-        />
-        {formik.touched.contact && formik.errors.contact?<p style={{color:"red"}}>{formik.errors.contact}</p>:null}  
-        <h2>Gender</h2>
+    <div className="main">
+      <form className="form" onSubmit={formik.handleSubmit}>
+        <div>
+          <label>Name :</label>
+          <input
+            autoComplete="off"
+            type="text"
+            name="name"
+            value={formik.values.name}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="Enter Username "
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <span>{formik.errors.name}</span>
+          ) : null}
+        </div>
 
-        <input
-          type="radio"
-          name="gender"
-          value="Male"
-          onChange={formik.handleChange}
-        />
-        <label>Male</label>
+        <div>
+          <label>Email id :</label>
+          <input
+            autoComplete="off"
+            type="email"
+            name="email"
+            value={formik.values.email}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="Enter Email id "
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <span>{formik.errors.email}</span>
+          ) : null}
+        </div>
 
-        <input
-          type="radio"
-          name="gender"
-          value="Female"
-          onBlur={formik.handleBlur}
-          onChange={formik.handleChange}
-        />
-        <label>Female</label>
+        <div>
+          <label>Contact Number :</label>
+          <input
+            autoComplete="off"
+            type="contact"
+            name="contact"
+            value={formik.values.contact}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="Enter contact "
+          />
+          {formik.touched.contact && formik.errors.contact ? (
+            <span>{formik.errors.contact}</span>
+          ) : null}
+        </div>
 
-        <select name="country" onBlur={formik.handleBlur} onChange={formik.handleChange}>
-          <option value="">Select Country</option>
-          <option value="India">India</option>
-          <option value="America">America</option>
-          <option value="France">France</option>
-          <option value="Germany">Germany</option>
-        </select>
-        {formik.touched.country && formik.errors.country?<p style={{color:"red"}}>{formik.errors.country}</p>:null}  
+        <div>
+          <label>State :</label>
+          <select
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.state}
+            name="state"
+          >
+            <option value="0">Select State</option>
+            <option value="Maharastra">Maharastra</option>
+            <option value="Goa">Goa</option>
+            <option value="Delhi">Delhi</option>
+            <option value="Gujrat">Gujrat</option>
+            <option value="Orissa">Orissa</option>
+          </select>
+          {formik.touched.state && formik.errors.state ? (
+            <span>{formik.errors.state}</span>
+          ) : null}
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            name="gender"
+            onChange={formik.handleChange}
+            value="Male"
+            checked={formik.values.gender === "Male"}
+          />
+          <label className="gender">Male</label>
+          <input
+            type="radio"
+            name="gender"
+            onChange={formik.handleChange}
+            value="Female"
+            checked={formik.values.gender === "Female"}
+          />
+          <label className="gender">Female</label>
+        </div>
+
         <button type="submit">Submit</button>
       </form>
     </div>
