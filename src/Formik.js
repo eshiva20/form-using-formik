487ff5ef23424d623b8768575246f53e00ad2389
shiva <shiva@nimapinfotech.com>
2023-02-22 18:17:42 +0530

    used formik components

--- src/Formik.js
+++ src/Formik.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-const Formik = ({ editMode, setUserInput, userinput, getData, userId ,setEditMode}) => {
+const UserForm = ({
+  editMode,
+  setUserInput,
+  userinput,
+  getData,
+  userId,
+  setEditMode,
+}) => {
   const emailregex =
     /^([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)@([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)[\\.]([a-zA-Z]{2,9})$/;
   const contactRegex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
@@ -17,50 +24,46 @@ const Formik = ({ editMode, setUserInput, userinput, getData, userId ,setEditMod
     age: "",
   };
 
-  const formik = useFormik({
-    initialValues: Object.keys(userinput).length === 0 ? userSchema : userinput,
-    validationSchema: Yup.object({
-      name: Yup.string()
-        .min(3, "min 3 char required")
-        .max(10, "max 10 chars allowed")
-        .required("Name Required"),
-      email: Yup.string()
-        .matches(emailregex, "Enter valid mail id")
-        .required("Please Enter your email"),
-      contact: Yup.string()
-        .matches(contactRegex, "Enter Correct Contact number ")
-        .required("Contact Required"),
-      state: Yup.string().required("Select State"),
-      age: Yup.string().required("age is required !"),
-    }),
-    onSubmit: (values, { setFieldValues }) => {
-      setUserInput({
-        name: values.name,
-        email: values.email,
-        contact: values.contact,
-        gender: values.gender,
-        state: values.state,
-        age: values.age,
-      });
-      setFieldValues("gender", "");
-      console.log("values", values);
-    },
+  let validationSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "min 3 char required")
+      .max(10, "max 10 chars allowed")
+      .required("Name Required"),
+    email: Yup.string()
+      .matches(emailregex, "Enter valid mail id")
+      .required("Please enter your email"),
+    contact: Yup.string()
+      .matches(contactRegex, "Enter Correct Contact number ")
+      .required("Contact Required"),
+    // state: Yup.string().min(3).required("Select State"),
+    // age: Yup.string().required("age is required !"),
   });
 
-  console.log("initialValues in formik", formik.values);
+  let onSubmit = (values, { setFieldValues }) => {
+    console.log("onsubmit called")
+    setUserInput({
+      name: values.name,
+      email: values.email,
+      contact: values.contact,
+      gender: values.gender,
+      state: values.state,
+      age: values.age,
+    });
+  };
 
-  const handleAdd = async () => {
+  const handleAdd = async (values) => {
+    console.log("clickd");
     await axios
       .post("http://localhost:8080/posts", userinput)
       .then(() => {
         alert("User Registered");
         getData();
-        formik.values.contact = "";
-        formik.values.name = "";
-        formik.values.email = "";
-        formik.values.gender = "";
-        formik.values.state = "0";
-        formik.values.age = "";
+        values.contact = "";
+        values.name = "";
+        values.email = "";
+        values.gender = "";
+        values.state = "0";
+        values.age = "";
       })
       .catch((err) => console.log("postErr", err));
   };
@@ -70,13 +73,6 @@ const Formik = ({ editMode, setUserInput, userinput, getData, userId ,setEditMod
       .put(`http://localhost:8080/posts/${userId}`, userinput)
       .then(() => {
         getData();
-        // setEditMode(false)
-        // formik.values.contact = "";
-        // formik.values.name = "";
-        // formik.values.email = "";
-        // formik.values.gender = "";
-        // formik.values.state = "0";
-        // formik.values.age = "";
       })
       .catch((err) => console.log("postErr", err));
   };
@@ -92,143 +88,123 @@ const Formik = ({ editMode, setUserInput, userinput, getData, userId ,setEditMod
   }
 
   return (
-    <div>
-      <form className="form" onSubmit={formik.handleSubmit}>
+    <Formik
+      initialValues={
+        Object.keys(userinput).length === 0 ? userSchema : userinput
+      }
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      <Form className="form">
         <div className="row">
           <div>
             <label>Name :</label>
-            <input
+            <Field
               autoComplete="off"
               type="text"
               name="name"
-              value={formik.values.name}
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
               placeholder="Enter Username "
             />
-            {formik.touched.name && formik.errors.name ? (
-              <span>{formik.errors.name}</span>
-            ) : null}
+            <ErrorMessage name="name" />
           </div>
 
           <div>
             <label>Email id :</label>
-            <input
+            <Field
               autoComplete="off"
               type="email"
               name="email"
-              value={formik.values.email}
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
               placeholder="Enter Email id "
             />
-            {formik.touched.email && formik.errors.email ? (
-              <span>{formik.errors.email}</span>
-            ) : null}
+            <ErrorMessage name="email" />
           </div>
         </div>
 
         <div className="row">
           <div>
             <label>Contact Number :</label>
-            <input
+            <Field
               autoComplete="off"
               type="contact"
               name="contact"
-              value={formik.values.contact}
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
               placeholder="Enter contact "
             />
-            {formik.touched.contact && formik.errors.contact ? (
-              <span>{formik.errors.contact}</span>
-            ) : null}
+            <ErrorMessage name="contact" />
           </div>
 
-          <div className="radio-container">
+          {/* <div className="radio-container">
             <label>Select One :</label>
             <div>
-              <input
+              <Field
                 type="radio"
                 name="gender"
-                onChange={formik.handleChange}
                 value="Male"
                 checked={formik.values.gender === "Male"}
               />
               <label className="gender">Male</label>
             </div>
             <div>
-              <input
+              <Field
                 type="radio"
                 name="gender"
-                onChange={formik.handleChange}
                 value="Female"
                 checked={formik.values.gender === "Female"}
               />
               <label className="gender">Female</label>
             </div>
-          </div>
+          </div> */}
         </div>
 
-        <div className="row">
+        {/* <div className="row">
           <div>
             <label>State :</label>
-            <select
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.state}
-              name="state"
-            >
-              <option value="0">Select State</option>
+            <select {...formik.getFieldProps("state")} name="state">
+              <option value="">Select State</option>
               <option value="Maharastra">Maharastra</option>
               <option value="Goa">Goa</option>
               <option value="Delhi">Delhi</option>
               <option value="Gujrat">Gujrat</option>
               <option value="Orissa">Orissa</option>
             </select>
-            {formik.touched.state && formik.errors.state ? (
-              <span>{formik.errors.state}</span>
-            ) : null}
+            <ErrorMessage name="state" />
           </div>
 
           <div className="radio-container">
             <label>Select One :</label>
             <div>
-              <input
+              <Field
                 type="radio"
                 name="age"
-                onChange={formik.handleChange}
                 value="less than 20Age"
                 checked={formik.values.age === "less than 20Age"}
               />
               <label>Age {"<"} 20 </label>
             </div>
             <div>
-              <input
+              <Field
                 type="radio"
                 name="age"
-                onChange={formik.handleChange}
                 value="greater than 20Age"
                 checked={formik.values.age === "greater than 20Age"}
               />
               <label>Age {">"} 20</label>
             </div>
           </div>
-          {formik.touched.age && formik.errors.age ? (
-            <span>{formik.errors.age}</span>
-          ) : null}
-        </div>
+          <ErrorMessage name="age" />
+        </div> */}
 
         {editMode ? (
-          <button className="submit">Update</button>
+          <button type="submit" className="submit">
+            Update
+          </button>
         ) : (
           <button className="submit" type="submit">
             Add User
           </button>
         )}
-      </form>
-    </div>
+      </Form>
+    </Formik>
   );
 };
 
-export default Formik;
+export default UserForm;
